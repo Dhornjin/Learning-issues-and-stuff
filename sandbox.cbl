f70709ab7d18d9a9347787c2428755b47638f1b6
D.D <44321529+Dhornjin@users.noreply.github.com>
2018-11-08 09:18:33 +0100

    Update sandbox.cbl

--- sandbox.cbl
+++ sandbox.cbl
@@ -9,13 +9,17 @@
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
-       
+        77     long PIC 99.
+        77     grab PIC a(20).
         77 individu PIC a.
                77    nom PIC a(15) VALUE "Grougalouragran".
                77    adresse PIC a(30) VALUE "Ruelle des eliatrops".
        LINKAGE SECTION.    *>section link des variable de module
        PROCEDURE DIVISION.
-
+          
+      
+          len(grab) TO long.
+          DISPLAY long.
 
        MAIN-PROCEDURE.
             DISPLAY adresse.
